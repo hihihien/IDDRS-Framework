125987a0a15652924c38352b72f146189fd3540c
Hien Giang <gianghien2999@gmail.com>
2022-02-15 17:14:30 +0100

    add carousel using scss

--- draft1/js/app.js
+++ draft1/js/app.js
@@ -1,3 +1,5 @@
+
+// swipe arrow 
 const gap = 46;
 const carousel = document.getElementById("carousel"),
     content = document.getElementById("content1"),
@@ -26,4 +28,25 @@ next.addEventListener("click", e => {
   
   let width = carousel.offsetWidth;
   window.addEventListener("resize", e => (width = carousel.offsetWidth));
-  
+
+// carousel with button 
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".content1");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("e1")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("e2")) {
+      slides.style.transform = "translateX(-33.33333333333333%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains('e3')){
+      slides.style.transform = 'translatex(-66.6666666667%)';
+      e.target.classList.add('active');
+    }
+  }
+});
