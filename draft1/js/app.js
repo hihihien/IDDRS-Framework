1e0bcf58c5c74039dd2c2cd2fa0a9a1885d4bb51
Hien Giang <gianghien2999@gmail.com>
2022-02-16 02:41:16 +0100

    done carousel

--- draft1/js/app.js
+++ draft1/js/app.js
@@ -1,52 +1,49 @@
-
-// swipe arrow 
-const gap = 46;
-const carousel = document.getElementById("carousel"),
-    content = document.getElementById("content1"),
-    next = document.getElementById("next"),
-    prev = document.getElementById("prev");
-
-
-next.addEventListener("click", e => {
-    carousel.scrollBy(width + gap, 0);
-    if (carousel.scrollWidth !== 0) {
-      prev.style.display = "flex";
-    }
-    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-      next.style.display = "flex";
-    }
-  });
-  prev.addEventListener("click", e => {
-    carousel.scrollBy(-(width + gap), 0);
-    if (carousel.scrollLeft - width - gap <= 0) {
-      prev.style.display = "flex";
-    }
-    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-      next.style.display = "flex";
-    }
-  });
-  
-  let width = carousel.offsetWidth;
-  window.addEventListener("resize", e => (width = carousel.offsetWidth));
-
-// carousel with button 
-const buttonsWrapper = document.querySelector(".map");
-const slides = document.querySelector(".content1");
-
-buttonsWrapper.addEventListener("click", e => {
-  if (e.target.nodeName === "BUTTON") {
-    Array.from(buttonsWrapper.children).forEach(item =>
-      item.classList.remove("active")
-    );
-    if (e.target.classList.contains("e1")) {
-      slides.style.transform = "translateX(-0%)";
-      e.target.classList.add("active");
-    } else if (e.target.classList.contains("e2")) {
-      slides.style.transform = "translateX(-33.33333333333333%)";
-      e.target.classList.add("active");
-    } else if (e.target.classList.contains('e3')){
-      slides.style.transform = 'translatex(-66.6666666667%)';
-      e.target.classList.add('active');
-    }
+// owl carousel with dots
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    navText: ["<i class='align arrow' id='prev'></i>",
+    "<i <i class='align arrow' id='next'></i>"],
+    slideBy: 5,
+    items: 4,
+    margin: 0,
   }
+
+  );
 });
+
+
+
+
+
+
+
+// swipe arrow carousel
+// const gap = 46;
+// const carousel = document.getElementById("carousel"),
+//     content = document.getElementById("content1"),
+//     next = document.getElementById("next"),
+//     prev = document.getElementById("prev");
+
+
+// next.addEventListener("click", e => {
+//     carousel.scrollBy(width + gap, 0);
+//     if (carousel.scrollWidth !== 0) {
+//       prev.style.display = "flex";
+//     }
+//     if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+//       next.style.display = "flex";
+//     }
+//   });
+//   prev.addEventListener("click", e => {
+//     carousel.scrollBy(-(width + gap), 0);
+//     if (carousel.scrollLeft - width - gap <= 0) {
+//       prev.style.display = "flex";
+//     }
+//     if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+//       next.style.display = "flex";
+//     }
+//   });
+  
+//   let width = carousel.offsetWidth;
+//   window.addEventListener("resize", e => (width = carousel.offsetWidth));
